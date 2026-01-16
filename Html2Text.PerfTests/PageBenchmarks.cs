46e447b540ac225b9df8024c546fac0fa8a8df75
pavlosmcg <pavlosmcg@users.noreply.github.com>
2026-01-16 15:13:09 +1300

    Add regression tests

--- Html2Text.PerfTests/PageBenchmarks.cs
+++ Html2Text.PerfTests/PageBenchmarks.cs
@@ -1,5 +1,4 @@
 ﻿using BenchmarkDotNet.Attributes;
-using System.Text;
 
 namespace Html2Text.PerfTests;
 
---
+++ Html2Text.RegressionTests/Html2TextTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Html2Text.RegressionTests;
+
+[TestFixture]
+public class Html2TextTests
+{
+    public static IEnumerable<TestCaseData> HtmlFiles()
+    {
+        foreach (var file in Directory.GetFiles("Samples", "*.html"))
+        {
+            yield return new TestCaseData(file)
+                .SetName($"Html2Text_Returns_ExpectedOutputFor({Path.GetFileName(file)})")
+                .SetCategory("Regression");
+        }
+    }
+
+    [Test]
+    [TestCaseSource(nameof(HtmlFiles))]
+    public async Task Html2Text_Returns_ExpectedOutputFor(string filePath)
+    {
+        // arrange
+        string input = File.ReadAllText(filePath);
+
+        // act
+        string text = Html2Text.Convert(input);
+
+        // assert
+        await Verify(text)
+            .UseParameters(Path.GetFileNameWithoutExtension(filePath));
+    }
+}
