0b70a6a67c4374af2a40de59f6fd50f6bbd14ccd
Graham <GrahamTheCoder@gmail.com>
2026-01-25 16:04:59 +0000

    Prepare to handle older frameworks

---
+++ Html2Text/Compatibility/StringBuilderBufferWriter.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Buffers;
+using System.Text;
+
+namespace Html2Text.Compatibility;
+
+/// <summary>
+/// A wrapper for StringBuilder to IBufferWriter for older frameworks that do not have 
+/// ArrayBufferWriter or when we want to capture output back into a StringBuilder.
+/// 
+/// Targets: net462, netstandard2.0
+/// Can be removed when support for these frameworks is dropped.
+/// </summary>
+internal class StringBuilderBufferWriter(StringBuilder builder) : IBufferWriter<char>
+{
+    private char[]? _buffer;
+
+    public void Advance(int count)
+    {
+        if (_buffer != null)
+        {
+            builder.Append(_buffer, 0, count);
+        }
+    }
+
+    public Memory<char> GetMemory(int sizeHint = 0) => throw new NotSupportedException();
+
+    public Span<char> GetSpan(int sizeHint = 0)
+    {
+        int size = sizeHint > 0 ? sizeHint : 256;
+        if (_buffer == null || _buffer.Length < size)
+        {
+            _buffer = new char[size];
+        }
+        return _buffer;
+    }
+}
